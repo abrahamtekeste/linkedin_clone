b787b0cf193b4db5d9edb4399358252d2f42a692
abrtek <abrtek1992@gmail.com>
2020-09-21 16:53:26 +0200

    appbar created

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // primaryColor: Color.fromRGBO(114, 118, 168, 0.8),
+        // brightness: Brightness.dark,
+        primaryColor: Color(0xFF0e76a8),
+        primaryColorBrightness: Brightness.dark,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
